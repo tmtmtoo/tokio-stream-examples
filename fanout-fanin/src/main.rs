46ac36b79b345309998f24b51731b055acf7c072
tmtmtoo <tmtmt2209@gmail.com>
2018-11-09 18:36:57 +0900

    refactor(streams): directories

--- fanout-fanin/src/main.rs
+++ fanout-fanin/src/main.rs
@@ -19,10 +19,10 @@ fn main() -> Result<(), RuntimeError> {
     let mut rt = Runtime::new().map_err(|_| RuntimeError::Executor)?;
 
     let basis = Instant::now();
-    let timer = source::timer(basis.clone(), 500);
-    let (counter_in, counter_out) = transform::counter();
-    let (elapsed_time_in, elapsed_time_out) = transform::elapsed_time(basis);
-    let stdout = sink::stdout();
+    let timer = source::timer::new(basis.clone(), 500);
+    let (counter_in, counter_out) = transform::counter::new();
+    let (elapsed_time_in, elapsed_time_out) = transform::elapsed_time::new(basis);
+    let stdout = sink::stdout::new();
 
     rt.spawn(cleanup(timer.forward(counter_in.fanout(elapsed_time_in))))
         .spawn(cleanup(
